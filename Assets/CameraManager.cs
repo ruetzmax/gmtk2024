fa25489f89d5ea61f8986e53fbd201cf8e3c8938
H3rrMelon3 <max.ruetz02@gmail.com>
2024-08-18 11:00:24 +0200

    camera

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 shipPosition = GameManager.instance.shipObject.transform.position;
+        transform.position = new Vector3(shipPosition.x, shipPosition.y, -10);
+    }
+}
